3c547ab63ebbf9a0b5c471150aaba6a1d2c2e9c7
GgMons <84619769+GgMons@users.noreply.github.com>
2021-06-11 13:14:28 +0200

    feat: backend working

--- README.md
+++ README.md
@@ -1,3 +1,100 @@
 #  Random Number Generator 
 
 This is a simple fullstack random number generator program developed by ggmons (me) during tha Lacerba node and typescript development course
+
+## Install
+
+```bash
+npm install
+```
+
+## Usage backend
+
+```bash
+ts-node main.ts
+```
+
+exemple output:
+
+```
+🦐 Ruota estratta 🍙
+{
+  "Bari": [
+    26,
+    76,
+    59,
+    29,
+    69
+  ],
+  "Cagliari": [
+    96,
+    57,
+    48,
+    17,
+    84
+  ],
+  "Firenze": [
+    83,
+    36,
+    75,
+    1,
+    5
+  ],
+  "Genova": [
+    24,
+    34,
+    8,
+    36,
+    96
+  ],
+  "Milano": [
+    77,
+    17,
+    70,
+    88,
+    21
+  ],
+  "Napoli": [
+    58,
+    33,
+    57,
+    53,
+    60
+  ],
+  "Palermo": [
+    30,
+    60,
+    40,
+    43,
+    91
+  ],
+  "Roma": [
+    60,
+    22,
+    65,
+    72,
+    44
+  ],
+  "Torino": [
+    8,
+    21,
+    64,
+    97,
+    56
+  ],
+  "Venezia": [
+    71,
+    44,
+    10,
+    74,
+    75
+  ],
+  "Nazionale": [
+    4,
+    90,
+    66,
+    47,
+    70
+  ]
+}
+```
--- main.ts
+++ main.ts
@@ -1,1 +1,48 @@
-console.log('Hello 😱 world');
+console.log('\t\t🦐 Ruota estratta 🍙');
+function RNG(min: number, max: number){
+  const rng = Math.random();
+  return Math.trunc(rng * (max-min) + min);
+  // rng * (max - min) + min
+  // rng = 0 => min
+  // rng = 1 => max
+}
+
+function RNGSequence(len: number, min: number, max: number){
+  if(len>max-min){
+    throw new Error(`\tCannot find ${len} numbers between ${min} and ${max}`);
+  }
+  const res: number[] = [];
+  while(res.length < len){
+    const rn = RNG(min,max);
+    if(res.includes(rn)){
+      continue;
+    }
+    res.push(rn);
+  }
+return res;
+}
+const route = ['Bari', 'Cagliari', 'Firenze', 'Genova', 'Milano', 'Napoli', 'Palermo', 'Roma', 'Torino', 'Venezia', 'Nazionale'];
+const estrazioni: {[ruota: string]: number[]} = {};
+
+for(const ruota of route){
+  const estrazione = RNGSequence(5,1,100);
+  estrazioni[ruota] = estrazione; 
+}
+console.log(JSON.stringify(estrazioni, null, 2));
+
+//console.log(`\t ${ruota} ${myArr}`);
+
+//function rngDec(min: number, max: number, precision: number){
+  //if(Number.isInteger(precision)){
+    //throw new Error('Precision must be an integer number');
+  //} else if(precision < 0){
+    //throw new Error('Precision must be a positive number');
+  //}
+  //const pot = Math.pow(10, precision);
+  //return  RNG(min * pot, max * pot)/pot;
+//}
+//console.log('\t⛷ 🇮🇹 Numeri random con decimali 🚀🇹🇷 🌊');
+//console.log(`\t\t\t ${rngDec(5, 10, 0)}`);
+//console.log(`\t\t\t ${rngDec(5, 10, 3.5)}`);
+//console.log(`\t\t\t ${rngDec(5, 10, -4)}`);
+//console.log(`\t\t\t ${rngDec(5, 10, -2.9)}`);
